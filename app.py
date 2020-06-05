e5e57cebda64345ffe17de4cb8a548676c06f3a4
andershup <anders242@gmail.com>
2020-06-05 17:36:39 +0000

    Added base.html in order to control flow and avoid repeating myself

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid iport ObjectId
+from bson.objectid import ObjectId
 
+import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'task-manager'
-app.config[MONGO_URI] = 'mongodb+srv://AndersOlesen:Enormouspassword@cluster0-1tddr.azure.mongodb.net/tast_manager?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'task_manager'
+#What I did was comment out the code below and pasted a copy of it on line 12 so you could see the difference. Mainly being I put the words MONGO_URI in quotes and removed the
+# mongodb://localhost at the end
+#Please do not push to github until you add the uri to an environment variable
+# app.config["MONGO_URI"] = 'mongodb+srv://AndersOlesen:EnormousPassword@cluster0-1tddr.azure.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World .... again there.. mate....."
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get("PORT")),
             debug = True)
+
+
