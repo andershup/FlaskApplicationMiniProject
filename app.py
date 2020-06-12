3c0b431476dd020749801545bd3f40596467ad47
andershup <anders242@gmail.com>
2020-06-12 14:29:56 +0000

    asdfkjas;dlfkj

--- app.py
+++ app.py
@@ -8,31 +8,35 @@ import env
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-#What I did was comment out the code below and pasted a copy of it on line 12 so you could see the difference. Mainly being I put the words MONGO_URI in quotes and removed the
+# What I did was comment out the code below and pasted a copy of it on line 12 so you could see the difference. Mainly being I put the words MONGO_URI in quotes and removed the
 # mongodb://localhost at the end
-#Please do not push to github until you add the uri to an environment variable
+# Please do not push to github until you add the uri to an environment variable
 # app.config["MONGO_URI"] = 'mongodb+srv://AndersOlesen:EnormousPassword@cluster0-1tddr.azure.mongodb.net/task_manager?retryWrites=true&w=majority'
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 
-
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", 
+    return render_template("tasks.html",
                             tasks=mongo.db.tasks.find())
+
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html', 
-                            categories=mongo.db.categories.find()) 
+    return render_template('addtask.html',
+                            categories=mongo.db.categories.find())
+
+
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks=mongo.db.tasks
+    tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+
 
 @app.route('/edit_task/ <task_id>')
 def edit_task(task_id):
@@ -40,12 +44,22 @@ def edit_task(task_id):
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task, categories=all_categories)
 
+
 @app.route('/update_task/ <task_id>', methods=["POST"])
-def 
+def update_task(task_id):
+    tasks = mongo.db.tasts
+    tasks.update({"_id": ObjectId(task_id)},
+    {
+       'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    
+    })
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get("PORT")),
+            port = int(os.environ.get("PORT")),
             debug = True)
-
-
